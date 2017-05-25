0a3449001329bcedc8186e8778cbbcccb86142a4
Daniel Kiser <dskiser@email.uark.edu>
2017-05-25 12:29:52 -0500

    created 404.html and 500.html in budget_keeper/templates/ and modified settings.py accordingly

--- budget_keeper/settings.py
+++ budget_keeper/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'budget_keeper.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [''],
+        'DIRS': ['', os.path.join(BASE_DIR, 'budget_keeper/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
