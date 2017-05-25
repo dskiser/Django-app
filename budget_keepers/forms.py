0e0476fdfb544afb6fbee4b0bd7ff85851e86777
Daniel Kiser <dskiser@email.uark.edu>
2017-05-25 13:48:52 -0500

    added bootstrap3 to edit_expense.html, changed labels on forms to make them more understandable

--- budget_keepers/forms.py
+++ budget_keepers/forms.py
@@ -6,29 +6,29 @@ class CreateBudget(forms.ModelForm):
 	class Meta:
 		model = Budget
 		fields = ['year','month','income','status']
-		labels = {'year': 'year', 'month': 'month', 'income':'income',
+		labels = {'year': 'year', 'month': 'month', 'income':'income for the month',
 				'status':'status'}
 		
 class EditCategory(forms.ModelForm):
 	class Meta:
 		model = Category
 		fields = ['name', 'budget']
-		labels = {'name': 'category', 'budget': 'budget'}
+		labels = {'name': 'category', 'budget': 'amount to budget'}
 		
 class BudgetStatus(forms.ModelForm):
 	class Meta:
 		model = Budget
 		fields = ['month','year','income','status']
-		labels = {'month': 'month', 'year':'year', 'status':'change budget status', 'income':'income'}
+		labels = {'month': 'month', 'year':'year', 'status':'change budget status', 'income':'income for the month'}
 		
 class EnterExpense(forms.ModelForm):
 	class Meta:
 		model = Expense
 		fields = ['expense','category','tag']
 		labels = {
-			'expense': 'expense', 
+			'expense': 'amount spent', 
 			'category': 'category',
-			'tag': 'tag',
+			'tag': 'tag (optional - can be vendor, reason for purchase, subcategory, etc)',
 			}
 	def __init__(self, budget=None, **kwargs):
 		super(EnterExpense, self).__init__(**kwargs)
